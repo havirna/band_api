881efc5c49f719525cbd23709055250b9ab69d08
Virna Harri <harrivirna18@gmail.com>
2021-12-13 14:41:54 +0100

    Built a basic API in Rails.

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :bands
+
+    namespace :api do
+    namespace :v1 do
+      resources :bands do
+        resources :members
+      end
+    end
+
+    namespace :v2 do
+      resources :members
+      resources :bands
+    end
+  end
+end
